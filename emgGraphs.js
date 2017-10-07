d1783b98e93366f6f18a91a8a840339179b7e1de
Brian Luebbert-Hill <BLuebbertHill@gmail.com>
2017-10-07 14:50:34 -0400

    Made the ddl

--- emgGraphs.js
+++ emgGraphs.js
@@ -1,3 +1,42 @@
+//Knockout components - controls the MVVM stuff
+
+var selectedScale = ko.observable();
+// Overall viewmodel for this screen, along with initial state
+
+function Scale(id, name, data) {
+	this.scaleId = id;
+	this.scaleName = name;
+	this.scaleData = data;
+}
+var masterScales = [
+    new Scale(0, "C", [48, 50, 52, 53, 55, 57, 59, 60]),
+    new Scale(1, "C#", [49, 51, 53, 54, 56, 58, 60, 61]),
+    new Scale(2, "D", [50, 52, 54, 55, 57, 59, 61, 62]),
+    new Scale(3, "D#", [51, 53, 55, 56, 58, 60, 62, 63]),
+    new Scale(4, "E", [52, 54, 56, 57, 59, 61, 63, 64]),
+    new Scale(5, "F", [53, 55, 57, 58, 60, 62, 64, 65]),
+    new Scale(6, "F#", [54, 56, 58, 59, 61, 63, 65, 66]),
+    new Scale(7, "G", [55, 57, 59, 60, 62, 64, 66, 67]),
+    new Scale(8, "G#", [56, 58, 60, 61, 63, 65, 67, 68]),
+    new Scale(9, "A", [57, 59, 61, 62, 64, 66, 68, 69]),
+    new Scale(10, "A#", [58, 60, 62, 63, 65, 67, 69, 70]),
+    new Scale(11, "B", [59, 61, 63, 64, 66, 68, 70, 71]),
+    new Scale(12, "Major Pentatonic", [54, 56, 58, 61, 63, 66]),
+    new Scale(13, "Minor Pentatonic", [51, 53, 56, 58, 61, 63])
+    ];
+
+function ViewModel() {
+	var self = this;
+
+    // Non-editable catalog data - would come from the server
+    self.scales = masterScales;   
+
+    // Computed data
+     
+
+    // Operations
+}
+
 //This tells Myo.js to create the web sockets needed to communnicate with Myo Connect
 
 
@@ -14,7 +53,7 @@ Myo.on('connected', function(){
 		onsuccess: function() { 
 			MIDI.setVolume(0, 127);
 			MIDI.noteOn(0,50,127,0);	}	
-	})
+		})
 	
 })
 
@@ -22,11 +61,11 @@ Myo.connect('com.myojs.emgGraphs');
 
 
 var rawData = [0,0,0,0,0,0,0,0];
-var scale = [54, 56, 58, 61, 63, 66]
+//var scale = [54, 56, 58, 61, 63, 66]
 var atRest = false;
 Myo.on('rest', function(){
 	atRest = !atRest;
- })
+})
 Myo.on('emg', function(data){
 	rawData = data;
 })
@@ -37,15 +76,16 @@ var resolution = 50;
 var emgGraphs;
 
 var graphData= [
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
-	Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0)
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0),
+Array.apply(null, Array(resolution)).map(Number.prototype.valueOf,0)
 ]
+
 
 $(document).ready(function(){
 
@@ -68,13 +108,12 @@ $(document).ready(function(){
 		}).data("plot");
 	});
 
-
 });
 
 var formatFlotData = function(data){
-		return [data.map(function(val, index){
-				return [index, val]
-			})]
+	return [data.map(function(val, index){
+		return [index, val]
+	})]
 }
 
 
@@ -87,10 +126,14 @@ var updateGraph = function(emgData){
 		emgGraphs[index].setData(formatFlotData(graphData[index]));
 		emgGraphs[index].draw();
 		for (var i = 0; i < 8  && !atRest; i+= 3)
-			MIDI.noteOn(0, scale[(128 + rawData[i]) % scale.length - 1],127,0);
+			MIDI.noteOn(0, masterScales[$('#ddl').val()].scaleData[(128 + rawData[i]) % masterScales[$('#ddl').val()].scaleData.length - 1],127,0);
 	})
 
 }
+
+
+
+
 
 /*
 
